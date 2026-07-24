bd35825c0da38ede426407b7dfc51a02a850c79b
jeeva-pakkiya <Mr.jeevapakkiyaraj@gmail.com>
2026-07-24 13:30:54 +0530

    day 2

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
